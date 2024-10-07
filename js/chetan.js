fbc701b342e16871f619f29909cb251d6f7b03ce
Cnikalje <nikaljechetan092@gmail.com>
2024-10-07 11:28:54 +0530

    today learning java script
    inline
    internal
    and external

---
+++ js/chetan.js
@@ -0,0 +1,27 @@
+function chetan(){
+    alert(4+5)
+    console.log("hello chetan");
+    document.write("lonavala")
+}
+
+document.write("I love MH<br>");
+document.write("I love MH<br>");
+document.write("I love MH<br>");
+document.write("I love MH<br>");
+document.write("I love MH<br>");
+
+var mytext=" I love india, i am indian";
+var test="yes i am chetan";
+document.write(mytext);
+document.write("<br>");
+document.write(mytext);
+document.write("<br>");
+document.write(mytext);
+document.write("<br>");
+document.write(test);
+document.write("<br>");
+document.write(mytext);
+document.write("<br>");
+document.write(mytext);
+document.write("<br>");
+document.write(test)
